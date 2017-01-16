7b3ab52b0710c3629b39116f8d27b57862db69d8
rodp15 <rodri.peralta.rp98@gmail.com>
2017-01-16 12:45:08 -0300

    Lunes

--- blog/views.py
+++ blog/views.py
@@ -3,5 +3,5 @@ from .models import Post
 from django.utils import timezone
 
 def post_list(request):
-    posts = Post.object.all()
+    posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts':posts})
